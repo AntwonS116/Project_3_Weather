845aa983fb815f0b3a191118125e69747a4e950b
AnthonyStevens1 <astevens600@gmail.com>
2021-11-20 20:32:53 -0500

    test

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,11 +1,11 @@
 var myMap = L.map("map", {
-    center: [37.6872, -97.3301],
-    zoom: 4.5
-  });
+  center: [37.6872, -97.3301],
+  zoom: 4.5
+});
 
-  // Adding the tile layer
+// Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 
@@ -14,38 +14,31 @@ const json = "static/resources/weather.json";
 
 d3.json(json).then(function(data) {
 
-  console.log(data);
+console.log(data);
 
-  var heatArray = [];
+var heatArray = [];
 
-  for (var i = 0; i < data.metadata.length; i++) {
-    var location = [data.metadata[i].Lat,data.metadata[i].Lng]
-    var city = [data.metadata[i]["City"]]
-    var maxtemp = [data.metadata[i]["Max_Temp"]]
-    var humidity = [data.metadata[i]["Humidity"]]
-    var wind = [data.metadata[i]["Wind Speed"]]
-    L.marker(location,{
-      opacity: 0.8,
-      fillOpacity: 0.75,
-      weight: 0.5,
-      color: 'black',
-      fillColor: 'green',
-      radius: 10000
-    }).bindPopup(`<p align = "left"> <strong>City:</strong> ${city} <br>
-    <strong>Temperature:</strong> ${maxtemp} <br> <strong>Humidity:</strong> ${humidity} <br> <strong>Wind Speed: </strong> ${wind} </p>`).addTo(myMap)
+for (var i = 0; i < data.metadata.length; i++) {
+  var location = [data.metadata[i].Lat,data.metadata[i].Lng]
+  var city = [data.metadata[i]["City"]]
+  var maxtemp = [data.metadata[i]["Max_Temp"]]
+  var humidity = [data.metadata[i]["Humidity"]]
+  var wind = [data.metadata[i]["Wind Speed"]]
+  L.marker(location,{
+    opacity: 0.8,
+    fillOpacity: 0.75,
+    weight: 0.5,
+    color: 'black',
+    fillColor: 'green',
+    radius: 10000
+  }).bindPopup(`<p align = "left"> <strong>City:</strong> ${city} <br>
+  <strong>Temperature:</strong> ${maxtemp} <br> <strong>Humidity:</strong> ${humidity} <br> <strong>Wind Speed: </strong> ${wind} </p>`).addTo(myMap)
 
-  
 
-    if (location) {
-      heatArray.push([location[0],location[1],maxtemp]);
-    }
+
+  if (location) {
+    heatArray.push([location[0],location[1],maxtemp]);
   }
+}
 
-
- ///   var heat = L.heatLayer(heatArray, {
-     /// max: Math.max(location[2]),
-     /// radius: 35,
-      ///blur:30,
-       //// }).addTo(myMap);  
- 
 });

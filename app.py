feb2147a138e28ab3cc02979ab38972dc58b853b
AnthonyStevens1 <astevens600@gmail.com>
2021-11-22 17:24:02 -0500

    heroku test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from logging import debug
 from flask_pymongo import PyMongo
 from werkzeug.utils import append_slash_redirect, redirect
 from script import api_call
-import easygui
+# import easygui
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def home():
 @app.route("/script")
 def script():
     api_call()
-    easygui.msgbox("Database Updated Successfully!", "Success")
+    # easygui.msgbox("Database Updated Successfully!", "Success")
     return redirect("/")
 
 @app.route("/heatmap")
